9f8fa9c9a6963b183551e21405b95672492af610
Kacper Krupinski <kkrup@softserveinc.com>
2020-09-21 10:50:16 +0200

    Implement authentication with JWT usage.

--- django_shop/urls.py
+++ django_shop/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
+
+from django_shop.shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('hello/', views.HelloView.as_view(), name='hello')
 ]
